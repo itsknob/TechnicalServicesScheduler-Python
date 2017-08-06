3a28a57ffa98f2a8b4e97279203e584945dc2825
itsknob <itsknob@gmail.com>
2017-08-06 11:44:53 -0400

    Converted classes properties to use private variable

--- TechTypes.py
+++ TechTypes.py
@@ -12,18 +12,140 @@ class EmployeeType(Enum):
     MANAGER = 2
 
 
+class PersonalInformation:
+
+    @property
+    def name_first(self):
+        return self._name_first
+    @name_first.setter
+    def name_first(self, new_name_first):
+        self._name_first = new_name_first
+
+    @property
+    def name_last(self):
+        return self._name_last
+    @name_last.setter
+    def name_last(self, new_name_last):
+        self._name_last = new_name_last
+
+    @property
+    def email(self):
+        return self._email
+    @email.setter
+    def email(self, new_email):
+        self._email = new_email
+
+    @property
+    def phone_number(self):
+        return self._phone_number
+    @phone_number.setter
+    def phone_number(self, new_phone_number):
+        self._phone_number = new_phone_number
+
+
+class WorkInformation:
+
+    @property
+    def student_id(self):
+        return self._student_id
+    @student_id.setter
+    def student_id(self, new_id):
+        self._student_id = new_id
+
+    @property
+    def job_type(self):
+        return self._job_type
+    @job_type.setter
+    def job_type(self, new_job_type=EmployeeType):
+        self._job_type = new_job_type
+
+    @property
+    def date_hire(self):
+        return self._date_hire
+    @date_hire.setter
+    def date_hire(self, new_date_hire):
+        self._date_hire = new_date_hire
+
+    @property
+    def date_graduate(self):
+        return self._date_graduate
+    @date_graduate.setter
+    def date_graduate(self, new_date_graduate):
+        self._date_graduate = new_date_graduate
+
+    @property
+    def shirt_size(self):
+        return self._shirt_size
+    @shirt_size.setter
+    def shirt_size(self, new_shirt_size):
+        self._shirt_size = new_shirt_size
+
+    @property
+    def notes(self):
+        return self._notes
+    @notes.setter
+    def notes(self, new_notes):
+        self._notes = new_notes
+
+
+class Student:
+    """
+    Contains Personal and Work-related information about the student
+
+    Requires PersionalInformation and WorkInformation Objects on Initialization
+    """
+
+    """
+    trainings_completed = {"training_aud_sound": False, "training_aud_lights": False, "training_mobile_sound": False, "training_mobile_lights": False, "training_stage_safety": False, "training_commuter_cafe": False, "training_woodland_commons": False, "training_grand_reading_room": False, "training_professionalism": False, "training_x32": False, "training_sound_consoles": False, "training_sound_design": False, "training_amp_speaker_matching": False, "training_advanced_ion": False, "training_lighting_design": False, "training_networking": False, "training_equiptment_repair": False, "training_scenery_shop": False}
+    """
+
+    def __init__(self, p_info=PersonalInformation, w_info=WorkInformation, trainings=list):
+        self.personal_information = p_info
+        self.work_information = w_info
+        self.trainings_completed = trainings
+
+    def get_full_name(self):
+        return self.personal_information.name_first.append(" " + self.personal_information.name_last)
+
+    def get_personal_info_as_list(self):
+        personal_info_list = list()
+        personal_info_list.append(self.personal_information.name_first)
+        personal_info_list.append(self.personal_information.name_last)
+        personal_info_list.append(self.personal_information.email)
+        personal_info_list.append(self.personal_information.phone_number)
+        return personal_info_list
+
+    def get_work_info_as_list(self):
+        work_info_list = list()
+        work_info_list.append(self.work_information.job_type)
+        work_info_list.append(self.work_information.date_hire)
+        work_info_list.append(self.work_information.date_graduate)
+        work_info_list.append(self.work_information.shirt_size)
+        work_info_list.append(self.work_information.notes)
+        return work_info_list
+
+    def return_all_info_as_list(self):
+        all_info_list = list()
+        temp1 = self.get_personal_info_as_list()
+        temp2 = self.get_work_info_as_list()
+        all_info_list.extend(temp1)
+        all_info_list.extend(temp2)
+        return all_info_list
+
+
 class Event:
     """Holds information about Events"""
 
     def __init__(self, name, location, date, start_time, end_time, number_employees, req_manager):
-        self.__event_name = name
-        self.__event_location = location
-        self.__event_date = date
-        self.__event_start_time = start_time
-        self.__event_end_time = end_time
-        self.__event_number_employees = number_employees
-        self.__event_req_manager = req_manager
+        self.event_name = name
+        self.event_location = location
+        self.event_date = date
+        self.event_start_time = start_time
+        self.event_end_time = end_time
+        self.event_number_employees = number_employees
+        self.event_req_manager = req_manager
 
+    # "Getters/Setters" - Required Attributes
     @property
     def event_name(self):
         """Name of Event"""
@@ -74,7 +196,7 @@ class Event:
     def event_requires_manager(self, new_requires):
         self.event_requires_manager = new_requires
 
-
+    # "Getters/Setters" - Additional Attributes
     @property
     def event_employee_list(self):
         return self.event_employee_list
@@ -89,115 +211,9 @@ class Event:
     def event_manager(self, new_manager):
         self.event_manager = new_manager
 
+    # Event Methods
+    def add_employee_to_event(self, employee=Student):
+        self.event_employee_list.append(employee)
 
-class PersonalInformation:
-
-    @property
-    def name_first(self):
-        return self.name_first
-    @name_first.setter
-    def name_first(self, new_name_first):
-        self.name_first = new_name_first
-
-    @property
-    def name_last(self):
-        return self.name_last
-    @name_last.setter
-    def name_last(self, new_name_last):
-        self.name_last = new_name_last
-
-    @property
-    def email(self):
-        return self.email
-    @email.setter
-    def email(self, new_email):
-        self.email = new_email
-
-    @property
-    def phone_number(self):
-        return self.phone_number
-    @phone_number.setter
-    def phone_number(self, new_phone_number):
-        self.phone_number = new_phone_number
-
-class WorkInformation:
-
-    @property
-    def job_type(self):
-        return self.job_type
-    @job_type.setter
-    def job_type(self, new_job_type=EmployeeType):
-        self.job_type = new_job_type
-
-    @property
-    def date_hire(self):
-        return self.date_hire
-    @date_hire.setter
-    def date_hire(self, new_date_hire):
-        self.date_hire = new_date_hire
-
-    @property
-    def date_graduate(self):
-        return self.date_graduate
-    @date_graduate.setter
-    def date_graduate(self, new_date_graduate):
-        self.date_graduate = new_date_graduate
-
-    @property
-    def shirt_size(self):
-        return self.shirt_size
-    @shirt_size.setter
-    def shirt_size(self, new_shirt_size):
-        self.shirt_size = new_shirt_size
-
-    @property
-    def notes(self):
-        return self.notes
-    @notes.setter
-    def note(self, new_notes):
-        self.note = new_notes
-
-
-class Student:
-    """
-    Contains Personal and Work-related information about the student
-
-    Requires PersionalInformation and WorkInformation Objects on Initialization
-    """
-
-    """
-    trainings_completed = {"training_aud_sound": False, "training_aud_lights": False, "training_mobile_sound": False, "training_mobile_lights": False, "training_stage_safety": False, "training_commuter_cafe": False, "training_woodland_commons": False, "training_grand_reading_room": False, "training_professionalism": False, "training_x32": False, "training_sound_consoles": False, "training_sound_design": False, "training_amp_speaker_matching": False, "training_advanced_ion": False, "training_lighting_design": False, "training_networking": False, "training_equiptment_repair": False, "training_scenery_shop": False}
-    """
-
-    def __init__(self, p_info=PersonalInformation, w_info=WorkInformation, trainings=list):
-        self.personal_information = p_info
-        self.work_information = w_info
-        self.trainings_completed = trainings
-
-    def get_full_name(self):
-        return self.personal_information.name_first.append(" " + self.personal_information.name_last)
-
-    def get_personal_info_as_list(self):
-        personal_info_list = list()
-        personal_info_list.append(self.personal_information.name_first)
-        personal_info_list.append(self.personal_information.name_last)
-        personal_info_list.append(self.personal_information.email)
-        personal_info_list.append(self.personal_information.phone_number)
-        return personal_info_list
-
-    def get_work_info_as_list(self):
-        work_info_list = list()
-        work_info_list.append(self.work_information.job_type)
-        work_info_list.append(self.work_information.date_hire)
-        work_info_list.append(self.work_information.date_graduate)
-        work_info_list.append(self.work_information.shirt_size)
-        work_info_list.append(self.work_information.notes)
-        return work_info_list
-
-    def return_all_info_as_list(self):
-        all_info_list = list()
-        temp1 = self.get_personal_info_as_list()
-        temp2 = self.get_work_info_as_list()
-        all_info_list.append(temp1)
-        all_info_list.append(temp2)
-        return all_info_list
+    def remove_employee_from_event(self, employee=Student):
+        self.event_employee_list.remove(employee)
